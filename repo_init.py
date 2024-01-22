9e85d5d95a2ac03cc3c19fdc460eb57ef3b38c2b
abhi <abhi@example.com>
2024-01-22 10:28:44 +0100

    made changes in repo_init.py. Added discovery_repository fn. to check valid .git folder present inside the repo or not

--- repo_init.py
+++ repo_init.py
@@ -1,21 +1,24 @@
 import pygit2
+from pygit2 import discover_repository
 from pygit2.repository import Repository
-import os
+import os, sys
 
-inp = input("Enter directory name: ")
+args = sys.argv
 
-if inp == "":
-    print("Please specify the name!")
+if len(args) < 2:
+    print("Please specify the name of repo!")
     os._exit(1)
 
-path = os.path.abspath("./")
-dirs = os.listdir(path)
+inp = args[1]
 
-if inp in dirs:
-    print(inp+" directory already exists!")
+git_path = os.path.abspath("./") + "/" + inp
+
+# Check if git repo exists. discovery_respository checks for .git folder also
+isPresent = discover_repository(git_path)
+
+if isPresent:
+    print(inp+" git repo already exists!")
     os._exit(1)
-
-git_path = path + "/" +inp
 
 # Git init
 ans = pygit2.init_repository(git_path, False)
@@ -26,6 +29,7 @@ with open(git_cb_path, "w") as f:
     f.write(inp)
     f.close()
 
+# Create a repository object. Create a TreeBuilder to write a tree.
 repo = Repository(git_path)
 treeObj = repo.TreeBuilder()
 tobj = treeObj.write()
