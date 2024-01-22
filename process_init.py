1a7b6647079c3de80d9fc49770c86f06ed78c120
abhi <abhi@example.com>
2024-01-22 13:16:16 +0100

    added process_init.py

---
+++ process_init.py
@@ -0,0 +1,21 @@
+from pygit2.repository import Repository
+from pygit2 import discover_repository, Signature
+import os
+import sys
+from subprocess import call
+
+path = os.path.abspath("./")
+print(path)
+
+args = sys.argv
+
+if len(args) < 2:
+    print("Please specify the process name!")
+    os._exit(1)
+
+p_name = args[1]
+
+git_path = path + "/" + p_name
+call(["python3", path+"/pyGitScripts/repo_init.py",p_name])
+os.chdir(git_path)
+call(["python3",path+"/pyGitScripts/author_init.py",p_name])
